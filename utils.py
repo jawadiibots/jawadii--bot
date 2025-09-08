16688b66c827521c5877bfd5cc433cdd4c802f46
Jawadii <itzjawad1527@gmail.com>
2025-09-08 00:08:04 -0700

    utils.py

--- utils.py
+++ utils.py
@@ -1,15 +1,17 @@
+# utils.py
+
 import json
 
 def save_json(data, filename):
-    with open(filename, 'w') as f:
+    with open(filename, "w") as f:
         json.dump(data, f)
 
 def load_json(filename):
-    with open(filename, 'r') as f:
+    with open(filename, "r") as f:
         return json.load(f)
 
-def nice(text):
-    return str(text).upper()
+def nice(msg):
+    return f"*** {msg} ***"
 
-def compute_position_size(balance, risk):
-    return balance * risk
+def compute_position_size(balance, risk_percent):
+    return balance * risk_percent / 100
